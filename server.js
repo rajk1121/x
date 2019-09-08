939d72835d2b179c6a4002b7be1838c2bc6e0a83
rajk1121 <rajat.mishra1121@gmail.com>
2019-09-08 22:05:54 +0530

    v1.1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./index.js')
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Server listening at port " + port);
 })
